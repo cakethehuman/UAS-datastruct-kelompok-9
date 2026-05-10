a4a9619f47ab5f47d6c71b3482b4209db42ce227
cakethehuman <willsonputra17@gmail.com>
2026-05-10 21:15:53 +0700

    end out the day

--- TTC.java
+++ TTC.java
@@ -3,7 +3,7 @@ class Game{
     char[][] board = new char[3][3];
     
     // Show baord to start the game ig
-    public void ShowBoard(){
+    public void showBoard(){
         System.err.println("Board : ");
         for (int i = 0; i < 3; i++) {
             System.out.print('|');
@@ -19,60 +19,75 @@ class Game{
         }
     }
 
-    public void InsertItem(int i, int j, char input){
+    public void insertItem(int i, int j, char input){
         board[i][j] = input;
     }
 
-    public void StartGame(){
+    public void checkWin(){
+
+    }
+
+    public boolean startGame(){
+        // need variable to play the game
         int counter = 0;
+
+        // if win lose logic ig
+        if(board[1][1] == 'x'){
+            return true;
+        }
+        // input starting values
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
                 counter += 1;
                 board[i][j] = (char) (counter + '0');
             }
         }
+        // show the board and inputs
+        showBoard();
         Scanner inputer = new Scanner(System.in);
         System.out.print("Mau masukin di mana? : ");
     
         int position = inputer.nextInt(); 
+        inputer.close();
         switch (position) {
             case 1:
-                InsertItem(0, 0, 'x');
+                insertItem(0, 0, 'x');
                 break;
             case 2:
-                InsertItem(0, 1, 'x');
+                insertItem(0, 1, 'x');
                 break;
             case 3:
-                InsertItem(0, 2, 'x');
+                insertItem(0, 2, 'x');
                 break;
             case 4:
-                InsertItem(1, 0, 'x');
+                insertItem(1, 0, 'x');
                 break;
             case 5:
-                InsertItem(1, 1, 'x');
+                insertItem(1, 1, 'x');
                 break;
             case 6:
-                InsertItem(1, 2, 'x');
+                insertItem(1, 2, 'x');
                 break;
             case 7:
-                InsertItem(2, 0, 'x');
+                insertItem(2, 0, 'x');
                 break;
             case 8:
-                InsertItem(2, 1, 'x');
+                insertItem(2, 1, 'x');
                 break;
             case 9:
-                InsertItem(2, 2, 'x');
+                insertItem(2, 2, 'x');
                 break;
             default:
                 throw new AssertionError();
         }
+        return startGame();
     }
 }
 public class TTC {
     public static void main(String[] args) {
         Game ttc = new Game();
-        ttc.StartGame();
+        ttc.startGame();
         // ttc.InsertItem(1, 0, 'x');
-        ttc.ShowBoard();
+        ttc.showBoard();
     }
 }
