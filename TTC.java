2474d8d751b94cb437e37a8e451e3e2e91188601
cakethehuman <willsonputra17@gmail.com>
2026-05-12 21:39:19 +0700

    update

--- TTC.java
+++ TTC.java
@@ -1,4 +1,4 @@
-import java.util.Scanner;
+import java.util.*;
 class Game{
     char[][] board = new char[3][3];
     
@@ -27,14 +27,52 @@ class Game{
 
     }
 
-    public boolean startGame(){
+    public void insertPosition(int position){
+        try{
+            switch (position) {
+            case 1:
+                insertItem(0, 0, 'X');
+                break;
+            case 2:
+                insertItem(0, 1, 'X');
+                break;
+            case 3:
+                insertItem(0, 2, 'X');
+                break;
+            case 4:
+                insertItem(1, 0, 'X');
+                break;
+            case 5:
+                insertItem(1, 1, 'x');
+                break;
+            case 6:
+                insertItem(1, 2, 'X');
+                break;
+            case 7:
+                insertItem(2, 0, 'X');
+                break;
+            case 8:
+                insertItem(2, 1, 'X');
+                break;
+            case 9:
+                insertItem(2, 2, 'X');
+                break;
+            default:
+                throw new AssertionError();
+            }
+            
+        } catch(Exception e){
+            System.out.println("Error lah");
+        }
+    }
+
+    public void startGame(){
         // need variable to play the game
         int counter = 0;
 
         // if win lose logic ig
-        if(board[1][1] == 'x'){
-            return true;
-        }
+  
+
         // input starting values
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
@@ -42,45 +80,16 @@ class Game{
                 board[i][j] = (char) (counter + '0');
             }
         }
-        // show the board and inputs
-        showBoard();
+        // show the board and inputs using a method so everything is seperated
         Scanner inputer = new Scanner(System.in);
-        System.out.print("Mau masukin di mana? : ");
-    
-        int position = inputer.nextInt(); 
-        inputer.close();
-        switch (position) {
-            case 1:
-                insertItem(0, 0, 'x');
-                break;
-            case 2:
-                insertItem(0, 1, 'x');
-                break;
-            case 3:
-                insertItem(0, 2, 'x');
-                break;
-            case 4:
-                insertItem(1, 0, 'x');
-                break;
-            case 5:
-                insertItem(1, 1, 'x');
-                break;
-            case 6:
-                insertItem(1, 2, 'x');
-                break;
-            case 7:
-                insertItem(2, 0, 'x');
-                break;
-            case 8:
-                insertItem(2, 1, 'x');
-                break;
-            case 9:
-                insertItem(2, 2, 'x');
-                break;
-            default:
-                throw new AssertionError();
+        while (!board.equals("XXX")) {
+            showBoard();
+            System.out.print("Mau masukin di mana? : ");
+            int position = inputer.nextInt();
+            insertPosition(position);   
         }
-        return startGame();
+        
+        inputer.close();
     }
 }
 public class TTC {
